25795ad7dd2d0067649829bace798b72eacc7b7d
Scott Addie <scott.addie@microsoft.com>
2019-10-02 13:26:36 -0500

    Refactoring

--- src/ContosoLending.Ui/Services/ExchangeRateService.cs
+++ src/ContosoLending.Ui/Services/ExchangeRateService.cs
@@ -1,19 +1,21 @@
-﻿using ContosoLending.Grpc;
+﻿using ContosoLending.Grpc.Server;
 using Grpc.Net.Client;
-using static ContosoLending.Grpc.ExchangeRateManager;
+using static ContosoLending.Grpc.Server.ExchangeRateManager;
 
 namespace ContosoLending.Ui.Services
 {
     public class ExchangeRateService
     {
-        public double GetExchangeRate()
+        public double GetExchangeRate(
+            string currencyTypeFrom,
+            string currencyTypeTo)
         {
             using var channel = GrpcChannel.ForAddress("https://localhost:5002");
             var client = new ExchangeRateManagerClient(channel);
             var request = new ExchangeRateRequest
             {
-                CurrencyTypeFrom = "USD",
-                CurrencyTypeTo = "Lev",
+                CurrencyTypeFrom = currencyTypeFrom,
+                CurrencyTypeTo = currencyTypeTo,
             };
 
             ExchangeRateReply exchangeRate = client.GetExchangeRate(request);
